f636929d47bce68de77cf9d6c00c0852d54095d2
Yuri Campolongo <yuricampolongo@outlook.com>
2021-04-05 11:38:10 -0700

    Adding service to get crypto prices

--- src/api/controllers/currency_controller.go
+++ src/api/controllers/currency_controller.go
@@ -1,15 +1,23 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/yuricampolongo/crypto-monitoring/src/api/domain/nomics"
 	"github.com/yuricampolongo/crypto-monitoring/src/api/providers/nomics_provider"
 )
 
 func GetCryptoCurrencies(c *gin.Context) {
-	nomics_provider.GetCurrencies(nomics.CurrencyTickerRequest{
-		Ids:      c.Query("ids"),
-		Convert:  c.Query("convert"),
-		Interval: c.Query("interval"),
+	response, err := nomics_provider.GetCurrencies(nomics.CurrencyTickerRequest{
+		Ids:      c.Param("ids"),
+		Convert:  c.Param("convert"),
+		Interval: c.Param("interval"),
 	})
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, "An error occurred to get currencies")
+	}
+
+	c.JSON(http.StatusOK, response)
 }
